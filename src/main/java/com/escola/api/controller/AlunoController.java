95b3cee5f4f41043be8da8878e9670eebe8e8e37
Fabiana <fabiana.lua@gmail.com>
2024-03-16 13:16:00 -0300

    ESC-002 Criação do endpoint Post para cadastrar aluno na classe AlunoController

--- src/main/java/com/escola/api/controller/AlunoController.java
+++ src/main/java/com/escola/api/controller/AlunoController.java
@@ -1,9 +1,14 @@
 package com.escola.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.escola.api.model.Aluno;
 import com.escola.api.repository.AlunoRepository;
 
 @RestController
@@ -13,6 +18,10 @@ public class AlunoController {
     @Autowired
     private AlunoRepository alunoRepository;
 
-    
+    @PostMapping
+    public ResponseEntity<Aluno> cadastrarAluno(@RequestBody Aluno aluno) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+        .body(alunoRepository.save(aluno));
+    }
     
 }
