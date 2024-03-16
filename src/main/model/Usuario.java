d5d4e6b26cd21370a6a15ea02ab9f621099558b1
Fabiana <fabiana.lua@gmail.com>
2024-03-16 10:29:54 -0300

    ESC-001 Criação da classe model usuário @Embeblade

---
+++ src/main/model/Usuario.java
@@ -0,0 +1,30 @@
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+
+public class Usuario {
+    @Column(nullable = false)
+    private String nome;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @Column(nullable = false)
+    private LocalDate dataDeNascimento;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String telefone;
+    
+}
