ffc61a539c5b521be7ec3a950e0d4b2995d18d0a
Fabiana <fabiana.lua@gmail.com>
2024-03-16 14:45:46 -0300

    ESC-002 Criação do endpoint Delete para excluir o aluno no AlunoController

--- src/main/java/com/escola/api/controller/AlunoController.java
+++ src/main/java/com/escola/api/controller/AlunoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +62,13 @@ public class AlunoController {
 
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @DeleteMapping("/{idAluno}")
+    public ResponseEntity<String> excluirAluno(@PathVariable("idAluno") Long idAluno) {
+        alunoRepository.deleteById(idAluno);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+        .body("Aluno removido com sucesso!");
     }
 
     
