e507979786432f3e6aa31ccb326f48da4f8e1424
Fabiana <fabiana.lua@gmail.com>
2024-03-16 13:40:58 -0300

    ESC-002 Criação do endpoint Get para buscar aluno pelo id na classe AlunoContrller

--- src/main/java/com/escola/api/controller/AlunoController.java
+++ src/main/java/com/escola/api/controller/AlunoController.java
@@ -1,11 +1,13 @@
 package com.escola.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,13 @@ public class AlunoController {
     public ResponseEntity<List<Aluno>> alunos() {
         return ResponseEntity.status(HttpStatus.OK)
         .body(alunoRepository.findAll());
+    }
+
+    @GetMapping("/{idAluno}")
+    public ResponseEntity<Optional<Aluno>> buscarAlunoPeloId(@PathVariable("idAluno") Long idAluno) {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(alunoRepository.findById(idAluno));
+       
     }
     
 }
