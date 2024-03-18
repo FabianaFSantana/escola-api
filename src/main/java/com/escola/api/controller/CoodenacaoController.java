63514c4094d637ec66547b9e1753d9814dfb467a
Fabiana <fabiana.lua@gmail.com>
2024-03-18 20:57:16 -0300

    ESC-009 Criação do endpoint Get para buscar coordenador pelo id na classe CoordenacaoController

--- src/main/java/com/escola/api/controller/CoodenacaoController.java
+++ src/main/java/com/escola/api/controller/CoodenacaoController.java
@@ -1,11 +1,13 @@
 package com.escola.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +34,12 @@ public class CoodenacaoController {
         return ResponseEntity.status(HttpStatus.OK)
         .body(coordenacaoRepository.findAll());
 
+    }
+
+    @GetMapping("/{idCoordenacao}")
+    public ResponseEntity<Optional<Coordenacao>> buscarCoordenadorPeloId(@PathVariable("idCoordenacao") Long idCoordenacao) {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(coordenacaoRepository.findById(idCoordenacao));
     }
 
     
