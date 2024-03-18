f793aea4ea4e327783302fd4321d5fe27360014d
Fabiana <fabiana.lua@gmail.com>
2024-03-18 09:11:15 -0300

    ESC-008 Criação da classe Controller de Turma e do método Post para cadastrar a turma

---
+++ src/main/java/com/escola/api/controller/TurmaController.java
@@ -0,0 +1,26 @@
+package com.escola.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.escola.api.model.Turma;
+import com.escola.api.repository.TurmaRepository;
+
+@RestController
+@RequestMapping("/turma")
+public class TurmaController {
+    
+    @Autowired
+    private TurmaRepository turmaRepository;
+
+    @PostMapping
+    public ResponseEntity<Turma> cadastrarTurma(@RequestBody Turma turma) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+        .body(turmaRepository.save(turma));
+    }
+}
--- src/main/java/com/escola/api/model/Turma.java
+++ src/main/java/com/escola/api/model/Turma.java
@@ -2,8 +2,12 @@ package com.escola.api.model;
 
 
 
+import com.escola.api.constant.Turno;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,8 +25,15 @@ public class Turma {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idTurma;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String nomeDaTurma;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Turno turno;
+
+
+
 
 
 }
