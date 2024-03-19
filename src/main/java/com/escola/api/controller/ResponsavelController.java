c7b71a85519010fb26624304726488c087914c41
Fabiana <fabiana.lua@gmail.com>
2024-03-18 22:22:43 -0300

    ESC-010 Criação do endpoint Post para cadastrar responsável na classe ResponsavelController

--- src/main/java/com/escola/api/controller/ResponsavelController.java
+++ src/main/java/com/escola/api/controller/ResponsavelController.java
@@ -1,9 +1,14 @@
 package com.escola.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.escola.api.model.Responsavel;
 import com.escola.api.repository.ResponsavelRepository;
 
 @RestController
@@ -12,6 +17,12 @@ public class ResponsavelController {
 
     @Autowired
     private ResponsavelRepository responsavelRepository;
+
+    @PostMapping
+    public ResponseEntity<Responsavel> cadastrarResponsavel(@RequestBody Responsavel responsavel) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+        .body(responsavelRepository.save(responsavel));
+    }
 
     
     
