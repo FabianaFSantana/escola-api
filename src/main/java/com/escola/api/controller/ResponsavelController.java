e78b5ee22312a24c43bafdcb57fb1d2925fc2ff2
Fabiana <fabiana.lua@gmail.com>
2024-03-18 22:42:34 -0300

    ESC-10 Criação do endpoint Get para buscar responsável pelo id na classe ResponsavelController

--- src/main/java/com/escola/api/controller/ResponsavelController.java
+++ src/main/java/com/escola/api/controller/ResponsavelController.java
@@ -1,11 +1,13 @@
 package com.escola.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +34,14 @@ public class ResponsavelController {
         return ResponseEntity.status(HttpStatus.OK)
         .body(responsavelRepository.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Responsavel>> buscarResponsavelPeloId(@PathVariable("id") Long id) {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(responsavelRepository.findById(id));
+    }
+
+
 
 
     
