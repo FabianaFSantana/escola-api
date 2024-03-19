c0e4adfe1a197636ed6e6829eb690bddf1a02950
Fabiana <fabiana.lua@gmail.com>
2024-03-18 22:58:58 -0300

    ESC-010 Criação do endpoint Delete para excluir Responsável na classe ResponsavelContrller

--- src/main/java/com/escola/api/controller/ResponsavelController.java
+++ src/main/java/com/escola/api/controller/ResponsavelController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,6 +63,13 @@ public class ResponsavelController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> escluirResponsavel(@PathVariable("id") Long id) {
+        responsavelRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+        .body("Responsável excluído com sucesso!");
     }
 
 
