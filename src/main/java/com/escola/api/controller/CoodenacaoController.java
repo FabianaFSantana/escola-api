045f18c5c56f3affb06db51d5a896024da583c08
Fabiana <fabiana.lua@gmail.com>
2024-03-18 21:17:33 -0300

    ESC-009 Criação do endpoint Delete para exluir coordenador na classe CoordenacaoController

--- src/main/java/com/escola/api/controller/CoodenacaoController.java
+++ src/main/java/com/escola/api/controller/CoodenacaoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,6 +65,13 @@ public class CoodenacaoController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @DeleteMapping("/{idCoordenacao}")
+    public ResponseEntity<String> excluirCoordenador(@PathVariable("idCoordenacao") Long idCoordenacao) {
+        coordenacaoRepository.deleteById(idCoordenacao);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+        .body("Coordenador excluído com sucesso!");
     }
 
     
