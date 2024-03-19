a066224efc57265f2c71e14f10f98743ab082e4a
Fabiana <fabiana.lua@gmail.com>
2024-03-18 22:25:49 -0300

    ESC-010 Criação do endpoint Get para exibir lista de Responsáveis na classe ResponsavelController

--- src/main/java/com/escola/api/controller/ResponsavelController.java
+++ src/main/java/com/escola/api/controller/ResponsavelController.java
@@ -1,8 +1,11 @@
 package com.escola.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +27,12 @@ public class ResponsavelController {
         .body(responsavelRepository.save(responsavel));
     }
 
-    
+    @GetMapping
+    public ResponseEntity<List<Responsavel>> listarResponsaveis() {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(responsavelRepository.findAll());
+    }
+
+
     
 }
