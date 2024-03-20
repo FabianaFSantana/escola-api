28e8b8fda2b2386fefa6c76dab64a675489e290d
Fabiana <fabiana.lua@gmail.com>
2024-03-20 10:30:17 -0300

    ESC-005 Criação da classe BoletimService

---
+++ src/main/java/com/escola/api/service/BoltimService.java
@@ -0,0 +1,14 @@
+package com.escola.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.escola.api.repository.BoletimRepository;
+
+public class BoltimService {
+    @Autowired
+    private BoletimRepository boletimRepository;
+
+    //Método para calcular média do aluno:
+
+
+}
