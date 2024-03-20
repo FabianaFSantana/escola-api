dcca6fa11c236f405fe73eb4ef2f6e8963b8c4f6
Fabiana <fabiana.lua@gmail.com>
2024-03-19 23:33:00 -0300

    ESC-010 Criação do endpoint Post para cadastrar disciplina na classe DisciplinaController

--- src/main/java/com/escola/api/controller/DisciplinaController.java
+++ src/main/java/com/escola/api/controller/DisciplinaController.java
@@ -1,9 +1,14 @@
 package com.escola.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.escola.api.model.Disciplina;
 import com.escola.api.repository.DisciplinaRepository;
 
 @RestController
@@ -12,6 +17,12 @@ public class DisciplinaController {
 
     @Autowired
     private DisciplinaRepository disciplinaRepository;
-    
+
+    @PostMapping
+    public ResponseEntity<Disciplina> cadastrarDisciplina(@RequestBody Disciplina disciplina) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+        .body(disciplinaRepository.save(disciplina));
+    }
+
     
 }
