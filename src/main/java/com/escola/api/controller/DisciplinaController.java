ca791a831a5c50fcf4fcf807e83b118e89cbccf0
Fabiana <fabiana.lua@gmail.com>
2024-03-20 07:47:04 -0300

    ESC-010 Criação do endpoint Delete para excluir a disciplina na classe DisciplinaController

--- src/main/java/com/escola/api/controller/DisciplinaController.java
+++ src/main/java/com/escola/api/controller/DisciplinaController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,6 +59,13 @@ public class DisciplinaController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @DeleteMapping("/{idDisciplina}")
+    public ResponseEntity<String> excluirDisciplina(@PathVariable("idDisciplina") Long idDisciplina) {
+        disciplinaRepository.deleteById(idDisciplina);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+        .body("Disciplina excluída com sucesso!");
     }
 
     
