9bf68a6bdd33d8bc42cea9c608db4f89d578b1d2
Fabiana <fabiana.lua@gmail.com>
2024-03-19 23:58:28 -0300

    ESC-010 Criação do endpoint Get para exibir lista de disciplinas na classe DisciplinaController

--- src/main/java/com/escola/api/controller/DisciplinaController.java
+++ src/main/java/com/escola/api/controller/DisciplinaController.java
@@ -1,8 +1,13 @@
 package com.escola.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +28,17 @@ public class DisciplinaController {
         return ResponseEntity.status(HttpStatus.CREATED)
         .body(disciplinaRepository.save(disciplina));
     }
+
+    @GetMapping
+    public ResponseEntity<List<Disciplina>> exibirListaDasDisciplinas() {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(disciplinaRepository.findAll());
+    }
+
+    
+
+    
+
 
     
 }
