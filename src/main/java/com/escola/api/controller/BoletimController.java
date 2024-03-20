abb46f7b2f5f8b8a293cc998f6c3eff9388db79e
Fabiana <fabiana.lua@gmail.com>
2024-03-20 10:09:45 -0300

    ESC-005 Criação do endpoint Put para atualizar os dados do boletim na classe BoletimController

--- src/main/java/com/escola/api/controller/BoletimController.java
+++ src/main/java/com/escola/api/controller/BoletimController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,7 +42,25 @@ public class BoletimController {
         .body(boletimRepository.findById(idBoletim));
     }
 
-    
+    @PutMapping("/{idBoletim}")
+    public ResponseEntity<Boletim> atualizarDadosDoBoletim(@PathVariable("idBoletim") Long idBoletim,
+    @RequestBody Boletim boletim) {
+        Optional<Boletim> boletimOptional = boletimRepository.findById(idBoletim);
+
+        if (boletimOptional.isPresent()) {
+            Boletim boletimEncontrado = boletimOptional.get();
+
+            boletimEncontrado.setNota1(boletim.getNota1());
+            boletimEncontrado.setNota2(boletim.getNota2());
+            
+            return ResponseEntity.status(HttpStatus.OK)
+            .body(boletimRepository.save(boletimEncontrado));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 
     
 }
