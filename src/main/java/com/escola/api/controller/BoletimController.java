1063380da0794766fd8774b4511139652fe86253
Fabiana <fabiana.lua@gmail.com>
2024-03-20 09:57:43 -0300

    ESC-005 Criação do endpoint Get para buscar boletim pelo Id na classe BoletimController

--- src/main/java/com/escola/api/controller/BoletimController.java
+++ src/main/java/com/escola/api/controller/BoletimController.java
@@ -1,11 +1,13 @@
 package com.escola.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +34,14 @@ public class BoletimController {
         return ResponseEntity.status(HttpStatus.OK)
         .body(boletimRepository.findAll());
     }
+
+    @GetMapping("/{idBoletim}")
+    public ResponseEntity<Optional<Boletim>> buscarBoletimPeloId(@PathVariable("idBoletim") Long idBoletim) {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(boletimRepository.findById(idBoletim));
+    }
+
+    
 
     
 }
