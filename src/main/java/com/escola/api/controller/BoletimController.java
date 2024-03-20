bbd7806fdcf70cf95cd6fe0e88d867287f3070cd
Fabiana <fabiana.lua@gmail.com>
2024-03-20 10:14:05 -0300

    ESC-005 Criação do método Delete para excluir um boletim na classe BoletimController

--- src/main/java/com/escola/api/controller/BoletimController.java
+++ src/main/java/com/escola/api/controller/BoletimController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,6 +59,14 @@ public class BoletimController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @DeleteMapping("/{idBoletim}")
+    public ResponseEntity<String> excluirBoletim(@PathVariable("idBoletim") Long idBoletim) {
+        boletimRepository.deleteById(idBoletim);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+        .body("Boletim excluído com sucesso!");
+
     }
 
 
