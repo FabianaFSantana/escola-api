8de17251c128275056dfc61443794e0e5b65fa10
Fabiana <fabiana.lua@gmail.com>
2024-03-20 11:32:27 -0300

    ESC-005 Exclusão da classe MediaDto

--- src/main/java/com/escola/api/service/BoltimService.java
+++ src/main/java/com/escola/api/service/BoltimService.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.escola.api.dto.MediaDto;
 import com.escola.api.model.Boletim;
 import com.escola.api.repository.BoletimRepository;
 
@@ -12,15 +11,6 @@ public class BoltimService {
     @Autowired
     private BoletimRepository boletimRepository;
 
-    //Método para calcular média do aluno:
-
-   /*  public Double calcularMedia(Long idBoletim, MediaDto mediaDto) {
-        Optional<Boletim> bOptional = boletimRepository.findById(idBoletim);
-
-        if (bOptional.isPresent()) {
-            Boletim boletimEncontrado = bOptional.get();
-
-
-        }
-    } */
 }
+
+   
