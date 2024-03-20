d0f351c5d640add5343fa3118ffc8452c6dd491d
Fabiana <fabiana.lua@gmail.com>
2024-03-20 11:34:33 -0300

    ESC-005 Criação do Método para calcular a média no BoletimService

--- src/main/java/com/escola/api/service/BoltimService.java
+++ src/main/java/com/escola/api/service/BoltimService.java
@@ -11,6 +11,27 @@ public class BoltimService {
     @Autowired
     private BoletimRepository boletimRepository;
 
+     //Método para calcular média do aluno:
+
+     public Double calcularMedia(Long idBoletim, Boletim nota1, Boletim nota2) {
+        Optional<Boletim> bOptional = boletimRepository.findById(idBoletim);
+
+        if (bOptional.isPresent()) {
+            Boletim boletimEncontrado = bOptional.get();
+
+            Double novaMedia = (boletimEncontrado.getNota1() + boletimEncontrado.getNota2()) / 2;
+            boletimEncontrado.setMedia(novaMedia);
+
+            boletimRepository.save(boletimEncontrado);
+
+            return novaMedia;
+
+        } else {
+            return null;
+        }
+
+    } 
+
 }
 
    
