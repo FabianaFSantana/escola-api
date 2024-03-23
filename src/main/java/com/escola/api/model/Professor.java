2448e6a4c711a9f769567fe5346389b10a4f0c08
Fabiana <fabiana.lua@gmail.com>
2024-03-23 10:57:10 -0300

    ESC-011 Criação da classe model Professor e da classe ProfessorRepository

---
+++ src/main/java/com/escola/api/model/Professor.java
@@ -0,0 +1,33 @@
+package com.escola.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name ="professor")
+
+public class Professor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Embedded
+    private Usuario usuarioProfessor;
+
+    @Column(length = 100)
+    private String professor;
+
+    @Column(length = 50)
+    private String disciplina;
+
+    
+}
