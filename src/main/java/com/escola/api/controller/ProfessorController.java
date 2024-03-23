c95159c46e6493bcb376be78e824a6553ae93325
Fabiana <fabiana.lua@gmail.com>
2024-03-23 11:11:25 -0300

    ESC-011 Criação do endpoint Get para exibir professor pelo id

--- src/main/java/com/escola/api/controller/ProfessorController.java
+++ src/main/java/com/escola/api/controller/ProfessorController.java
@@ -2,11 +2,13 @@ package com.escola.api.controller;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +36,14 @@ public class ProfessorController {
       return ResponseEntity.status(HttpStatus.OK)
       .body(professorRepository.findAll());
    }
+
+   @GetMapping("/{id}")
+   public ResponseEntity<Optional<Professor>> exibirProfessorPeloUsuario(@PathVariable("id") Long id) {
+      return ResponseEntity.status(HttpStatus.OK)
+      .body(professorRepository.findById(id));
+   }
+
+   
    
  
 
