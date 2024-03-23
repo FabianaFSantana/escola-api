f122b497001e36eb8861a06370edbdd9e6aa67fe
Cayton-Benicio <cayton.dev@gmail.com>
2024-03-23 09:28:49 -0300

    ESC-007 Foi Adicionado o PostMapping na classe controller

--- src/main/java/com/esolca/api/controller/ComunicadoController.java
+++ src/main/java/com/esolca/api/controller/ComunicadoController.java
@@ -1,11 +1,67 @@
 package com.esolca.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.esolca.api.model.Comunicado;
+import com.esolca.api.repository.ComunicadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/comunicado")
 public class ComunicadoController {
+    @Autowired
+    private ComunicadoRepository comunicadoRepository;
+
+    @PostMapping
+    public ResponseEntity<Comunicado> cadastrarComunicado(@RequestBody Comunicado comunicado) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(comunicadoRepository.save(comunicado));
+    }
+}
+
+    /*@GetMapping
+    public ResponseEntity<List<Aluno>> alunos() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(alunoRepository.findAll());
+    }
+
+    @GetMapping("/{idAluno}")
+    public ResponseEntity<Optional<Aluno>> buscarAlunoPeloId(@PathVariable("idAluno") Long idAluno) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(alunoRepository.findById(idAluno));
+
+    }
+
+    @PutMapping("/{idAluno}")
+    public ResponseEntity<Aluno> atualizarDadosAluno(@PathVariable("idAluno") Long idAluno, @RequestBody Aluno aluno) {
+        Optional<Aluno> alunoOptional = alunoRepository.findById(idAluno);
+
+        if (alunoOptional.isPresent()) {
+            Aluno alunoAtualizado = alunoOptional.get();
+
+            alunoAtualizado.getUsuarioAluno().setNome(aluno.getUsuarioAluno().getNome());
+            alunoAtualizado.getUsuarioAluno().setDataDeNascimento(aluno.getUsuarioAluno().getDataDeNascimento());
+            alunoAtualizado.getUsuarioAluno().setEmail(aluno.getUsuarioAluno().getEmail());
+            alunoAtualizado.getUsuarioAluno().setTelefone(aluno.getUsuarioAluno().getTelefone());
+            alunoAtualizado.setMatricula(aluno.getMatricula());
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(alunoRepository.save(alunoAtualizado));
+
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @DeleteMapping("/{idAluno}")
+    public ResponseEntity<String> excluirAluno(@PathVariable("idAluno") Long idAluno) {
+        alunoRepository.deleteById(idAluno);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .body("Aluno removido com sucesso!");
+    }
 
 
 
