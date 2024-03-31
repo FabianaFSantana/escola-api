c7c7901236d138fa71efd4fede6e52e82bb06eb5
Fabiana <fabiana.lua@gmail.com>
2024-03-31 19:27:36 -0300

    ESC-003 Correção e ajustes da classe model Agenda e da classe AgendaRepository

--- src/main/java/com/escola/api/repository/AgendaRepository.java
+++ src/main/java/com/escola/api/repository/AgendaRepository.java
@@ -1,9 +1,11 @@
 package com.escola.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.escola.api.model.Agenda;
 
+@Repository
 public interface AgendaRepository extends JpaRepository<Agenda, Long> {
 
     
